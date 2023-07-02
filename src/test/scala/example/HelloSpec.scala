655cd834e79d1cc7896018c39d1ee8ec323a1dd1
Florian Witteler <github@flwi.de>
2023-07-02 16:32:27 +0200

    more tests

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -4,9 +4,17 @@ import cats.implicits._
 
 class HelloSpec extends munit.FunSuite {
 
-  test("test wrong usage of Either") {
+  test("first test") {
+    assertEquals(1, 1)
+  }
 
+  test("test wrong usage of Either.catchNonFatal") {
+    println("before wrong use of Either.catchNonFatal")
     Either.catchNonFatal(throw new IllegalAccessError("asdf"))
+    assertEquals(1, 1)
+  }
+
+  test("last test") {
     assertEquals(1, 1)
   }
 }
